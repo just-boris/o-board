9b07bbc575f58d153f35204b2497b3309f19fb1d
tormozz48 <tormozz48@gmail.com>
2015-04-10 21:53:38 +0300

    Implement sending change event with fields data

--- common.blocks/filter/filter.browser.js
+++ common.blocks/filter/filter.browser.js
@@ -1,6 +1,7 @@
 modules.define('filter', ['i-bem__dom'], function (provide, BEMDOM) {
     'use strict';
     provide(BEMDOM.decl(this.name, {
+
         onSetMod: {
             js: {
                 inited: function () {
@@ -17,14 +18,52 @@ modules.define('filter', ['i-bem__dom'], function (provide, BEMDOM) {
                             return this.findBlockInside(item, 'checkbox');
                         }, this)
                         .forEach(function (item) {
-                            item.bindTo('control', 'click', this._onChange, this);
+                            item.bindTo('control', 'click', this._onChange.bind(this), this);
                         }, this);
+
+                    this.findBlockInside('date-input', 'input').on('change', this._onChange, this);
                 }
             }
         },
 
         _onChange: function () {
-            console.log('CHANGE!');
+            var selectElems = [
+                    ['repo-select', 'repository'],
+                    ['label-select', 'label']
+                ],
+                checkboxElems = [
+                    ['issue-check', 'issues'],
+                    ['pr-check', 'pullRequests'],
+                    ['assign-check', 'assignToMe']
+                ],
+                values = selectElems
+                    .map(function (item) {
+                        return this.findBlockInside(item[0], 'select');
+                    }, this)
+                    .map(function (item) {
+                        return item.getVal();
+                    }, this)
+                    .map(function (item, index) {
+                        return { field: selectElems[index][1], value: item };
+                    })
+                    .concat(checkboxElems
+                    .map(function (item) {
+                        return this.findBlockInside(item[0], 'checkbox');
+                    }, this)
+                    .map(function (item, index) {
+                        return { field: checkboxElems[index][1], value: item.hasMod('checked') };
+                    }, this))
+                    .concat({
+                        field: 'date',
+                        value: this.findBlockInside('date-input', 'input').getVal()
+                    })
+                    .reduce(function (prev, item) {
+                        prev[item.field] = item.value;
+                        return prev;
+                    }, {});
+
+                console.log('filter data: ' + JSON.stringify(values));
+                this.emit('change', values);
         }
     }));
 });
