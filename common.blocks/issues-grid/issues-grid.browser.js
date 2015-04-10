5030ab1c957b1e33c1af77d0f206fc5e212eaa97
Konstantin Gladkikh <birhoff@yandex-team.ru>
2015-04-10 15:03:15 +0300

    Базовый блок грида

---
+++ common.blocks/issues-grid/issues-grid.browser.js
@@ -0,0 +1,8 @@
+/* global modules:false */
+
+modules.define('issues-grid', function(provide) {
+
+provide();
+
+});
+
--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,26 +1,28 @@
 //noinspection BadExpressionStatementJS
 ({
-    block : 'page',
-    title : 'Title of the page',
-    favicon : '/favicon.ico',
-    head : [
-        { elem : 'meta', attrs : { name : 'description', content : '' } },
-        { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
-        { elem : 'css', url : '_index.css' }
+    block: 'page',
+    title: 'Title of the page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'meta', attrs: { name: 'description', content: '' } },
+        { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
+        { elem: 'css', url: '_index.css' }
     ],
-    scripts: [{ elem : 'js', url : '_index.js' }],
-    mods : { theme : 'islands' },
-    content : [
+    scripts: [{ elem: 'js', url: '_index.js' }],
+    mods: { theme: 'islands' },
+    content: [
         {
             block: 'header',
             title: 'User\'s issues'
         },
         {
-            block : 'content',
-            content : [
+            block: 'content',
+            content: [
                 {
-                    block : 'spin',
-                    mods : { theme : 'islands', size : 'm', visible : true }
+                    block: 'spin',
+                    mods: { theme: 'islands', size: 'm', visible: true }
+                }, {
+                    block: 'issues-grid'
                 }
             ]
         }
